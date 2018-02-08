81fc1654fadacf342b918c5c91a4c65f6b2153e9
Italo Henrique Queiroz <italohqueiroz@gmail.com>
2018-02-08 09:03:44 -0200

    Initial Commit

---
+++ InAppExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InAppExample
+//
+//  Created by Italo Henrique Queiroz on 08/02/18.
+//  Copyright © 2018 Italo Henrique Queiroz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
